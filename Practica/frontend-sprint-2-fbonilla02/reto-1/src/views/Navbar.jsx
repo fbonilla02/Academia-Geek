bab3938c792dd706cf7e53d0ae8f4cb2ee2b2d18
fbonilla <franciscobonillac@gmail.com>
2023-02-07 15:08:28 -0500

    realice el navbar responsive y funcional

--- Practica/frontend-sprint-2-fbonilla02/reto-1/src/views/Navbar.jsx
+++ Practica/frontend-sprint-2-fbonilla02/reto-1/src/views/Navbar.jsx
@@ -11,19 +11,29 @@ import Button from '../components/Button'
 `
  const NavStyled = styled.nav`
     display: flex;
+
+    @media(max-width: 768px){
+      display: ${props => props.isMobileToggled ? 'none' : 'flex'};
+      position: absolute;
+      
+      width: 100%;
+      top: 60px;
+      left: 0;
+      flex-direction: column;
+      justify-content: center;
+      height: calc(100vh - 77px);
+      background-color: #f0efe9;
+      border-top: 1px solid black;
+    }
 `
   const Ul = styled.ul`
     display: flex;
+    flex-direction:  ${props => props.isMobileToggled ? 'row' : 'column'};
     align-items: center;
     list-style: none;
     padding: 0px ;
-    margin:  0 20px;
-    
-`
-
-  const LiNav = styled.li`
-    padding: 0px 10px;
-    font-weight: 300;
+    margin: ${props => props.isMobileToggled ? '0 20px' : '20px'}  ;
+    gap:  ${props => props.isMobileToggled ? '20px' : '1.5rem'};
 `
   const Hambur = styled.div`
     display: block;
@@ -35,26 +45,28 @@ import Button from '../components/Button'
 
 export default function Navbar() {
   const [isMobileToggled, setIsMobileToggled] = useState(false)
+
   console.log(isMobileToggled);
   return (
-    <NavbarStyled isMobileToggled={isMobileToggled}>
+    <NavbarStyled >
     <img src='https://res.cloudinary.com/dz6w87mmx/image/upload/v1675704220/Aerial/logo_rlqi0p.png' alt='logo Aerial' />
-    <NavStyled>
-    {isMobileToggled ? ('') : ''}
-        <Ul>
-            <LiNav>Job Search</LiNav>
-            <LiNav>About</LiNav>
-            <LiNav>Pricing</LiNav>
-            <LiNav>Contact</LiNav>
-        </Ul>
-        <Ul>
-            <LiNav><Button text="Log in"  /> </LiNav>
-            <LiNav><Button text="Try it Free" color= "#fff" backgroundColor='#24B26B' border='#24B26B'/></LiNav>
-        </Ul>
-        
-    </NavStyled><Hambur onClick={()=> setIsMobileToggled(!isMobileToggled)}>
+    <Hambur onClick={()=> setIsMobileToggled(!isMobileToggled)}>
           <img src='https://res.cloudinary.com/dz6w87mmx/image/upload/v1675790860/Aerial/menu_hamburger_svmbco.svg' alt='logo hambur'/>
         </Hambur>
+    <NavStyled isMobileToggled={isMobileToggled}>
+        <Ul isMobileToggled={isMobileToggled}>
+            <li>Job Search</li>
+            <li>About</li>
+            <li>Pricing</li>
+            <li>Contact</li>
+        </Ul>
+        <Ul isMobileToggled={isMobileToggled}>
+            <li ><Button text="Log in"  /> </li>
+            <li><Button text="Try it Free" color= "#fff" backgroundColor='#24B26B' border='#24B26B'/></li>
+        </Ul>
+        
+    </NavStyled>
+    
     </NavbarStyled>
   )
 }
